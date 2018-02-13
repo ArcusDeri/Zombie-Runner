fba3db98985ab5d521bf5d51a063abe8706c037d
Marcin <michniak@yandex.com>
2018-02-13 12:24:12 +0100

    Use two new keys to use in the game. V for "view" and H for "Helicopter". V zooms in and out by changing the player's field of view. H calls helicopter support. Create new gameobject for helicopter and place it outside the map. For now it prints in console a message when it is called. It can be called only once. Soon there will be played a sound when player presses H key.

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera MyEyes;
+	private float DefaultFOV;
+
+	// Use this for initialization
+	void Start () {
+		MyEyes = GetComponent<Camera> ();
+		DefaultFOV = MyEyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Zoom")) {
+			MyEyes.fieldOfView = DefaultFOV / 1.5f;
+		} else {
+			MyEyes.fieldOfView = DefaultFOV;
+		}
+	}
+}
---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip CallSound;
+
+	private bool IsCalled = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("CallHeli") && !IsCalled) {
+			IsCalled = true;
+			Debug.Log("Helicopter called.");
+		}
+	}
+}
