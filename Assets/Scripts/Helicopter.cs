fcee164917921f4220fd6c8fbb2b3396da3e30c8
Marcin <michniak@yandex.com>
2018-02-14 12:35:39 +0100

    Import "start pack" with assets from the course. Put the helicopter in the scene. Create two audio sources for helicopter. One for a 3D sound of the rotor movement and a 2D sound for a radio call sound.

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -7,17 +7,20 @@ public class Helicopter : MonoBehaviour {
 	public AudioClip CallSound;
 
 	private bool IsCalled = false;
+	private AudioSource MyAudioSource;
 
 	// Use this for initialization
 	void Start () {
-		
+		MyAudioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("CallHeli") && !IsCalled) {
 			IsCalled = true;
-			Debug.Log("Helicopter called.");
+			Debug.Log ("Helicopter called.");
+			MyAudioSource.clip = CallSound;
+			MyAudioSource.Play ();
 		}
 	}
 }
