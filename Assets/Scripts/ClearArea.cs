b8a9229a227582effca5015d5cbf82fc407d0ee2
Marcin <michniak@yandex.com>
2018-02-15 15:14:58 +0100

    Create box collider and attach it to the player. It is named ClearArea and it is used to check if the local area is clear for a helicopter landing. If there is no collision for one second, display message on a HUD.

---
+++ Assets/Scripts/ClearArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearArea : MonoBehaviour {
+
+	public float TimeSinceLastTrigger = 0f;
+
+	private GameObject HUD;
+
+	// Use this for initialization
+	void Start () {
+		HUD = GameObject.Find ("HUD");
+		HUD.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimeSinceLastTrigger += Time.deltaTime;
+		if (TimeSinceLastTrigger >= 1f) {
+			HUD.SetActive (true);
+		}
+	}
+
+	void OnTriggerStay(){
+		TimeSinceLastTrigger = 0f;
+	}
+}
