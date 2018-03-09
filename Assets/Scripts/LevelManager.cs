3500ec302ce8bc5235f5ff0ccfeb92d4b8ebb911
Marcin <michniak@yandex.com>
2018-03-09 13:44:52 +0100

    Create LevelManager and a script with the same name. It handles the button clicks and win/lose events. Add skeleton script for incoming buttons. Prefab the LevelManager. Attach the LevelManager to exit and start buttons. Game scene loads after clicking "Start".

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void OnClickStart(){
+		Debug.Log ("Load Game scene.");
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void OnClickMainMenu(){
+		Debug.Log ("Load Start scene.");
+		SceneManager.LoadScene("Start");
+	}
+
+	public void OnClickHighScores(){
+		Debug.Log ("Load Highscores scene.");
+		SceneManager.LoadScene ("Highscores");
+	}
+
+	public void OnClickExit(){
+		Debug.Log ("Exiting the game.");
+		Application.Quit();
+	}
+}
