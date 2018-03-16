aee65d6219d6f8917a1472829c7ffd3d6db4eba7
Marcin <michniak@yandex.com>
2018-03-16 23:09:07 +0100

    Adjust the Highscores scene to view the list of ten positions. Create method which fills in the list with scores from data.json. Works with empty file too.

--- Assets/Scripts/Highscores.cs
+++ Assets/Scripts/Highscores.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
+using UnityEngine.UI;
 
 public class Highscores : MonoBehaviour {
 
-	private static string FilePath = Path.Combine (Application.streamingAssetsPath,"data.JSON");
+	private static string FilePath;
 
 	// Use this for initialization
 	void Start () {
+		FilePath = Path.Combine (Application.streamingAssetsPath,"data.JSON");
+		UpdateScoreList ();
 	}
 
 	public static ScoreData LoadScores(){
+		FilePath = Path.Combine (Application.streamingAssetsPath,"data.JSON");
 		var jsonHighscores = File.ReadAllText (FilePath);
 		var scoreData = JsonUtility.FromJson<ScoreData> (jsonHighscores);
 		if (scoreData == null)
@@ -26,6 +31,7 @@ public class Highscores : MonoBehaviour {
 		roundScores = TrimScoreList (roundScores);
 
 		var highScoresJson = JsonUtility.ToJson (roundScores);
+		FilePath = Path.Combine (Application.streamingAssetsPath,"data.JSON");
 		File.WriteAllText (FilePath, highScoresJson);
 	}
 
@@ -44,5 +50,14 @@ public class Highscores : MonoBehaviour {
 			data.Scores = data.Scores.GetRange (0, 10);
 		}
 		return data;
+	}
+
+	private void UpdateScoreList(){
+		var scoreList = GameObject.Find ("ScoreList");
+		var positions = scoreList.GetComponentsInChildren<Text> ();
+		var scores = FormatScoreData(LoadScores ());
+		for (int i = 0; i < scores.Scores.Count; i++) {
+			positions [i].text = (i + 1).ToString() + "." + " " + scores.Scores[i].ToString() + " s.";
+		}
 	}
 }
